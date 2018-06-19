05055d7afce368cae7d0a115b45fc2d25763a9f5
Seethayya Nidadavolu <seethayya.n@HCL.COM>
2018-06-19 11:43:15 +0530

    added useHash property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,7 @@ const appRoutes : Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     HttpModule,
     HttpClientModule,
     MatButtonModule,
